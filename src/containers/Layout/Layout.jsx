e5bee9afd5ad16a9ea5d4e4b80271eaf314cf5de
Ing. Nelson GE <nescalan@gmail.com>
2022-04-04 17:02:53 -0600

    [UPDATE] Product Title: font size respoinsive

--- src/containers/Layout/Layout.jsx
+++ src/containers/Layout/Layout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
 import { Menu } from "../../components";
-import { ProductList } from "../index";
 
 const Layout = () => {
   return (
     <>
       <Menu />
-      {/* <ProductList /> */}
       <Outlet />
     </>
   );
